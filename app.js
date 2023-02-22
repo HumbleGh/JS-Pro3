130b2f346b8819ac4b152ceb810669f312a32d0b
Humble-Preacher <61693488+Humble-Preacher@users.noreply.github.com>
2023-02-22 16:15:09 +0000

    functionalities added

---
+++ app.js
@@ -0,0 +1,32 @@
+// initializing the value of counter
+let count = 0;
+
+// selecting value and buttons
+const value = document.getElementById("value");
+const btns = dovument.querySelectorAll(".btn");
+
+// Adding event listeners
+btns.forEach(function(btn) {
+    btn.addEventListener("click", function(e) {
+        const styles = e.currentTarget.classList;
+        if(styles.contains("decrease")) {
+            count--;
+        }else if (styles.contains("increase")) {
+            count++;
+        }else {
+            count = 0;
+        }
+
+        if (count > 0) {
+            value.style.color = "green";
+        }
+        if (count < 0) {
+            value.style.color = "red";
+        }
+        if (count === 1) {
+            value.style.color = "#222";
+        }
+
+        value.textContent = count;
+    });
+});
